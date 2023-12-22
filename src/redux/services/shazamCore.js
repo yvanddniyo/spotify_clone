d78359560ddd8f0c7a7472ab1a46523e727aef3d
yvanddniyo <nyvan80076@gmail.com>
2023-12-22 23:33:20 +0200

    first commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "2c61e09487msh15b2f45b73bd456p1b4d34jsne1e49a6b52ef",
+//     "X-RapidAPI-Host": "shazam.p.rapidapi.com",
+//   },
+// };
+
+// fetch("https://shazam.p.rapidapi.com/charts/track", options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam.p.rapidapi.com",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "2c61e09487msh15b2f45b73bd456p1b4d34jsne1e49a6b52ef"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/track" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
